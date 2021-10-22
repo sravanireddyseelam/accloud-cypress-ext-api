60f2a3d3cf15d6535d9fcb39e7c29cd8d33a3e47
Manuel Abascal <manuelsuarezabascal@gmail.com>
2021-10-22 11:57:59 -0400

    fix indentation & code styling

--- cypress/integration/api-employees/e2e-create-employees.js
+++ cypress/integration/api-employees/e2e-create-employees.js
@@ -1,10 +1,8 @@
-const createEmployeeFixture = require('../../fixtures/createEmployee.json')
-const updateEmployeeFixture = require('../../fixtures/updateEmployee.json')
+const createEmployeeFixture = require('../../fixtures/createEmployee.json');
+const updateEmployeeFixture = require('../../fixtures/updateEmployee.json');
 
-describe('Create a new employee', ()=>{
-
-    it('Creates a new employee', ()=>{
-
+describe('Create a new employee', () => {
+    it('Creates a new employee', () => {
         var employeeId = employeeId
         // Creates a new employee profile using POST /employees Ext API
         cy.request({
@@ -15,18 +13,13 @@ describe('Create a new employee', ()=>{
                 password: Cypress.env('password')
             },
             body: createEmployeeFixture
-            
-
-        }).then((employeeCreationResponse)=>{
-            expect(employeeCreationResponse.status).to.eql(201)
-            expect(employeeCreationResponse.body).has.property('id')
+        }).then((employeeCreationResponse) => {
+            expect(employeeCreationResponse.status).to.eql(201);
+            expect(employeeCreationResponse.body).has.property('id');
             
         }).then((employeeCreationResponse) => {
-
             var employeeId = employeeCreationResponse.body.id
-            cy.log("employee_id is " +  employeeId)
-            
-            
+            cy.log("employee_id is " +  employeeId) 
             // Fetches the newly created employee using GET /employees/{employee_id} Ext API
             cy.request({
                 method: 'GET',
@@ -37,35 +30,35 @@ describe('Create a new employee', ()=>{
 
                 },
     
-            }).then((employeeGetResponse)=>{
-                expect(employeeGetResponse.status).to.eql(200)
-                expect(employeeGetResponse.body).has.property('id').to.eql(employeeId)
-                expect(employeeGetResponse.body).has.property('external_id')
+            }).then((employeeGetResponse) => {
+                expect(employeeGetResponse.status).to.eql(200);
+                expect(employeeGetResponse.body).has.property('id').to.eql(employeeId);
+                expect(employeeGetResponse.body).has.property('external_id');
             
             }).then((employeeGetResponse) => {
-                var employeeExternalId = employeeGetResponse.body.external_id
-                cy.log("employee_id is " +  employeeExternalId)
-        }).then((employeeGetResponse) => {
+                var employeeExternalId = employeeGetResponse.body.external_id;
+                cy.log("employee_id is " +  employeeExternalId);
+            }).then((employeeGetResponse) => {
 
-            var employeeId = employeeGetResponse.body.id
+                var employeeId = employeeGetResponse.body.id;
 
-            // Updates the newly created employee using PUT /employees/{employee_id} Ext API
-            cy.request({
-                method: 'PUT',
-                url: `ext/api/v2/employees/employees/`+ employeeId,
-                auth: {
-                    username: Cypress.env('username'),
-                    password: Cypress.env('password')
+                // Updates the newly created employee using PUT /employees/{employee_id} Ext API
+                cy.request({
+                    method: 'PUT',
+                    url: `ext/api/v2/employees/employees/`+ employeeId,
+                    auth: {
+                        username: Cypress.env('username'),
+                        password: Cypress.env('password')
 
-                },
-                body: updateEmployeeFixture
+                    },
+                    body: updateEmployeeFixture
 
-            }).then((employeeUpdateResponse) => {
-                expect(employeeUpdateResponse.status).to.eql(200)
-                expect(employeeUpdateResponse.body).has.property('id').to.eql(employeeId)
-                expect(employeeUpdateResponse.body).has.property('external_id').to.eql(updateEmployeeFixture.external_id)
-            })
-        })
-    })
-})
-})
+                }).then((employeeUpdateResponse) => {
+                    expect(employeeUpdateResponse.status).to.eql(200);
+                    expect(employeeUpdateResponse.body).has.property('id').to.eql(employeeId);
+                    expect(employeeUpdateResponse.body).has.property('external_id').to.eql(updateEmployeeFixture.external_id);
+                });
+            });
+        });
+    });
+});
